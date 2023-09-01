956b02717a001abc726662202536cf01021ba49e
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2023-09-01 21:46:01 +1000

    added scene transition

---
+++ Assets/Scripts/GlobalState/GameManagerClient.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameManagerClient : MonoBehaviour
+{
+    // Only accessible by inheritors, to avoid messy access to global state.
+    protected GameManager GameManager { get; private set; }
+
+    private void Awake()
+    {
+        // Note: An alternative to this approach is to turn GameManager into a
+        // singleton. However, this might be more prone to abuse as a "global
+        // variable", since any script could mutate its state.
+        GameManager = GameObject
+            .FindWithTag(GameManager.Tag)
+            .GetComponent<GameManager>();
+    }
+}
